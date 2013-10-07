479a400168540e206d4c2ec3723b207c77d1b45e
Giacomo Neri <higiacomo@gmail.com>
2013-10-07 21:54:25 +0200

    activate debug via URL query string http://yoast.com/wordpress-debug/?debug=true

--- lib/utils.php
+++ lib/utils.php
@@ -12,3 +12,15 @@ function is_element_empty($element) {
   $element = trim($element);
   return empty($element) ? false : true;
 }
+
+// 
+// add ?debug=secret to add debug TRUE (from http://yoast.com/wordpress-debug/?debug=true)
+// 
+add_action( 'init', 'my_debug');
+function my_debug() {
+   if( current_user_can('update_plugins')
+       && isset( $_GET['debug'] )
+       && $_GET['debug'] == 'secret'
+       && !defined( 'WP_DEBUG' )
+   ) { define( 'WP_DEBUG', true ); }
+}
